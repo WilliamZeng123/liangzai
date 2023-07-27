349ab6dcfb3c858166f796367e26a6381e8ec589
WilliamZeng123 <136957658+WilliamZeng123@users.noreply.github.com>
2023-07-28 00:49:07 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=25684243&pid=8dec44c609a537d108b6909d2d0600a1&tid=7cca2ccc930e2db8fb870f86782627d7",
-    "https://www.maimemo.com/share/page?uid=25684243&pid=8dec44c609a537d108b6909d2d0600a1&tid=7cca2ccc930e2db8fb870f86782627d7",
-    "https://www.maimemo.com/share/page?uid=25684243&pid=8dec44c609a537d108b6909d2d0600a1&tid=7cca2ccc930e2db8fb870f86782627d7",
+    "https://www.maimemo.com/share/page?uid=25684243&pid=8c484dade01e7874a7411ea84f478000&tid=b23bb1a230fc376caee3a5e9fbba9f72",
+    "https://www.maimemo.com/share/page?uid=25684243&pid=8c484dade01e7874a7411ea84f478000&tid=b23bb1a230fc376caee3a5e9fbba9f72",
+    "https://www.maimemo.com/share/page?uid=25684243&pid=8c484dade01e7874a7411ea84f478000&tid=b23bb1a230fc376caee3a5e9fbba9f72",
   ]
   /**
    * 生成随机数字
